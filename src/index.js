e2261171b4b4cf90b4a0f03c7d6ed8b1bf9c5e2e
Tomas Padilla <padillatomasagustin@gmail.com>
2021-09-10 10:08:35 +0200

    Context for Filters

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./Components/App";
 import { ProductsProvider } from "./Context/products_context";
+import { FilterProvider } from "./Context/filter_context";
 
 ReactDOM.render(
   <ProductsProvider>
-    <App />
+    <FilterProvider>
+      <App />
+    </FilterProvider>
   </ProductsProvider>,
   document.getElementById("root")
 );
