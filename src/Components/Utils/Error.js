155372ea3bdc2fb75588743bf821a270be473bc9
Tomas Padilla <padillatomasagustin@gmail.com>
2021-09-15 10:35:15 +0200

    Error component, Products Views

--- src/Components/Utils/Error.js
+++ src/Components/Utils/Error.js
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
 const Error = () => {
   return (
-    <div className="section-center">
-      <h2>There was an Error</h2>
-    </div>
+    <ErrorContainer className="section-center">
+      There was an Error
+    </ErrorContainer>
   );
 };
+
+const ErrorContainer = styled.div`
+  width: 90%;
+  height: 20vh;
+  display: grid;
+  place-items: center;
+  font-family: var(--FontWork);
+  color: var(--ColorBlack-85);
+  font-size: 3.2rem;
+  font-weight: 300;
+  letter-spacing: 1.1px;
+`;
 
 export default Error;
